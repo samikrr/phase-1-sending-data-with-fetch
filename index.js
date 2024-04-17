40ed74eb02904e3b4f164869a2bc730d77e3cc6a
Samuel Kirori <samuelkirori300@gmail.com>
2024-04-17 15:49:20 +0300

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+//Send Data
+//Handle the response
+//Handle errors
+//Return the Fetch chain
+ 
+const submitData= () =>{
+    const formData ={
+        name: "Steve",
+        email: "steve@steve.com"
+    }
+    
+    const configObj= {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(formData)
+    };
+    return fetch("http://localhost:3000/users" ,configObj)
+    .then(response => response.json())
+    .then(data => {
+        document.body.textContent = data.id;
+    })
+    .catch(error => {
+        document.body.textContent = error.message;
+    });
+    };
